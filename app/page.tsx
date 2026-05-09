c1f6ce634bf51e2901fba8d2d01165b4846cd96c
MyFuckingBet <enzo.tosta08@gmail.com>
2026-05-09 10:04:31 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
   async function loginWithGoogle() {
     await supabase.auth.signInWithOAuth({
       provider: 'google',
-      options: { redirectTo: `${window.location.origin}` }
+      options: { redirectTo: `${window.location.origin}/auth/callback` }
     })
   }
 
